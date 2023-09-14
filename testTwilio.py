52141613d04727562d59299ebedc5ac3e3ea4e0b
noam-kaleidoo <noam.groshois@kaleidoo.ai>
2023-09-14 16:54:23 +0300

    EXPOSE 5000

--- testTwilio.py
+++ testTwilio.py
@@ -77,5 +77,6 @@ def whatsapp_reply():
         print("noam123")
     return str(resp)
 
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
